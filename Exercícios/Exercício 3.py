00043b4d17602d296ff4f3dbc6acf79dfe29258c
Victor Augusto <victor.ribeiro.silva30@escola.pr.gov.br>
2026-03-02 07:59:00 -0300

    fgfjjfjhhjhj
    jhhgdgfhgfhfg

--- Exercícios/Exercício 3.py
+++ Exercícios/Exercício 3.py
@@ -1,22 +1,24 @@
+import math as m
+
 x = int(input('Primeiro valor? '))
 
 y = int(input('Segundo valor? '))
 
-Operation = input('Qual operação? (+,-,*,/) ')
+Operation = input('Qual operação? (+,-,*,/, ^, √, !, ?, Nome encurtado da operação.) ')
 
-if (Operation == '+'):
+if (Operation == '+' or 'Add'):
 
     print(x+y)
 
-elif (Operation == '-'):
+elif (Operation == '-' or 'Minus'):
 
     print(x-y)
 
-elif (Operation == '*'):
+elif (Operation == '*' or 'Multi'):
 
     print(x*y)
 
-elif (Operation == '/'):
+elif (Operation == '/' or 'Diviz'):
 
     if y != 0:
 
@@ -25,6 +27,22 @@ elif (Operation == '/'):
     else:
 
         print('Erro Desconhecido: Divisão por zero é impossível.')
+
+if (Operation == '^' or 'Power'):
+
+    print(x.m.power(y))
+
+elif (Operation == '√' or 'Root'):
+
+    print(x.m.sqrt(y))
+
+elif (Operation == '!' or 'Factorial'):
+
+    print(x.m.factorial)
+
+elif (Operation == '?' or 'Termial'):
+
+    print(x*(x+1)/2)
 
 else:
 
