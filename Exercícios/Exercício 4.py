2a8c47439850e895d209d2cc0326fd6d645c9a18
Victor Augusto <victor.ribeiro.silva30@escola.pr.gov.br>
2026-03-11 10:36:06 -0300

    tetertret

--- Exercícios/Exercício 4.py
+++ Exercícios/Exercício 4.py
@@ -1,1 +1,2 @@
-# "C#"
+x = float(input('Digite um valor númerico aleatório para converter em metros, centímetros e milímetros: '))
+print('Metros = {} | Centímetros = {} | Milímetros = {}'.format(x, x*100, x*1000))
