465b5ee7267a336745047217b0db34d8c5e82472
Victor Augusto <victor.ribeiro.silva30@escola.pr.gov.br>
2026-03-16 12:24:57 -0300

    8p9oi7ytyuyçouiy

---
+++ Exercícios/Exercício 5.py
@@ -0,0 +1,13 @@
+h = float(input('Altura da parede? '))
+
+w = float(input('Largura da parede? '))
+
+a = h + w
+
+p1 = a*8.5
+
+p2 = a*4.25
+
+p3 = a*2.125
+
+print('1 demão = {}m² | 2 demãos = {}m² | 3 demãos = {}m²'.format(p1, p2, p3))
