b0916dc936fc47ec6daec5a2714610933d538ce7
Victor Augusto <victor.ribeiro.silva30@escola.pr.gov.br>
2026-03-17 12:02:10 -0300

    ilokjhgnb

--- Exercícios/Exercício 5.py
+++ Exercícios/Exercício 5.py
@@ -1,13 +1,14 @@
-h = float(input('Altura da parede? '))
+while True:
+ h = float(input('Altura da parede? '))
 
-w = float(input('Largura da parede? '))
+ w = float(input('Largura da parede? '))
 
-a = h + w
+ a = h + w
 
-p1 = a*8.5
+ p1 = a*8.5
 
-p2 = a*4.25
+ p2 = a*4.25
 
-p3 = a*2.125
+ p3 = a*2.125
 
-print('1 demão = {}m² | 2 demãos = {}m² | 3 demãos = {}m²'.format(p1, p2, p3))
+ print('1 demão = {}m² | 2 demãos = {}m² | 3 demãos = {}m²'.format(p1, p2, p3))
