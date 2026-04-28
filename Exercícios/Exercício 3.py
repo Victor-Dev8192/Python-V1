27147663bb1e54d21600cbba6e5c4c7bbef4bf6f
Victor Augusto <victor.ribeiro.silva30@escola.pr.gov.br>
2026-04-28 09:41:57 -0300

    3

--- Exercícios/Exercício 3.py
+++ Exercícios/Exercício 3.py
@@ -4,7 +4,7 @@ def Triangle_Number(x):
  return (int(x)*(int(x)+1))//2
 
 def Op():
- Op = input('Lista de operações abaixo:\n\n1) Adição.\n\n2) Subtração.\n\n3) Multiplicação.\n\n4) Divisão.\n\n5) Exponenciação.\n\n6) Raíz Quadrada.\n\n7) Fatorial.\n\n8) Termial\n\n9) Números Iguais.\n\n10) Números Diferentes.\n\n').capitalize().strip()
+ Op = input('Lista de operações abaixo:\n\n1) Adição.\n\n2) Subtração.\n\n3) Multiplicação.\n\n4) Divisão.\n\n5) Exponenciação.\n\n6) Raíz Quadrada.\n\n7) Fatorial.\n\n8) Termial\n\n9) Números Iguais.\n\n10) Números Diferentes.\n\nOperação Escolhida = ').capitalize().strip()
  try:
   x = float(input('\nPrimeiro valor? ').strip())
  except ValueError:
@@ -18,82 +18,82 @@ def Op():
    y = float(input('\nSegundo valor? ').strip())
   except ValueError:
    print('Erro 1: Número inválido.')
-  return
+   return
 
  match Op:
 
     case '1':
 
-      print(x+y)
+      print('\n', x+y)
       return
 
     case '2':
 
-      print(x-y)
+      print('\n', x-y)
       return
 
     case '3':
 
-     print(x*y)
+     print('\n', x*y)
      return
 
     case '4':
 
       if y != 0:
 
-        print(x/y)
+        print('\n', x/y)
         return
 
       else:
 
-        print('Erro 2: Divisão por zero é impossível.')
+        print('\nErro 2: Divisão por zero é impossível.')
         return
 
     case '5':
 
-     print(x**y)
+     print('\n', x**y)
      return
 
     case '6':
 
-     print(m.sqrt(int(x)))
+     print('\n', m.sqrt(int(x)))
      return
 
     case '7':
 
      if x >= 0:
 
-      print(m.factorial(int(x)))
+      print('\n', m.factorial(int(x)))
       return
 
      else:
      
-      print('Erro 3: Fatorial de números negativos é impossível.')
+      print('\nErro 3: Fatorial de números negativos é impossível.')
       return
 
     case '8':
 
-     print(Triangle_Number(x))
+     print('\n', Triangle_Number(x))
      return
 
     case '9':
 
-     print(x==y)
+     print('\n', x==y)
      return
 
     case '10':
 
-     print(x!=y)
+     print('\n', x!=y)
      return
 
     case _:
 
-     print('Erro 4: Operação inválida e/ou indisponível.')
+     print('\nErro 4: Operação inválida e/ou indisponível.')
      return
 
 while True:
  Op()
  Again = input('\nReiniciar? (Y/N): ').strip().upper()
  if Again != 'Y':
-  print('Tchau!')
+  print('\nTchau!')
   break
