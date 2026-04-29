0630399f22371b86b9d0cac89912fa408f196c71
Victor Augusto <victor.ribeiro.silva30@escola.pr.gov.br>
2026-04-29 08:13:52 -0300

    Formatação de números.

--- Exercícios/Exercício 3.py
+++ Exercícios/Exercício 3.py
@@ -3,12 +3,20 @@ import math as m
 def Triangle_Number(x):
  return (int(x)*(int(x)+1))//2
 
+def Number_Format(n):
+ if n == 0:
+  return '0'
+ 
+ if abs(n) >= 1e12 or abs(n) <= 1e-3:
+  return f'{n:.0e}'
+ return f'{n:,.2f}'.rstrip('0').rstrip('.')
+
 def Op():
  Op = input('Lista de operações abaixo:\n\n1) Adição.\n\n2) Subtração.\n\n3) Multiplicação.\n\n4) Divisão.\n\n5) Exponenciação.\n\n6) Raíz Quadrada.\n\n7) Fatorial.\n\n8) Termial\n\n9) Números Iguais.\n\n10) Números Diferentes.\n\nOperação Escolhida = ').strip()
  try:
   x = float(input('\nPrimeiro valor? ').strip())
  except ValueError:
-  print('Erro 1: Número inválido.')
+  print('\nErro 1: Número inválido.')
   return
  
  y = None
@@ -17,7 +25,7 @@ def Op():
   try:
    y = float(input('\nSegundo valor? ').strip())
   except ValueError:
-   print('Erro 1: Número inválido.')
+   print('\nErro 1: Número inválido.')
    return
 
  match Op:
@@ -25,16 +33,19 @@ def Op():
     case '1':
 
       Resultado = (x+y)
+      print('\n' + Number_Format(Resultado))
       return
 
     case '2':
 
       Resultado = (x-y)
+      print('\n' + Number_Format(Resultado))
       return
 
     case '3':
 
      Resultado = (x*y)
+     print('\n' + Number_Format(Resultado))
      return
 
     case '4':
@@ -42,6 +53,7 @@ def Op():
       if y != 0:
 
         Resultado = (x/y)
+        print('\n' + Number_Format(Resultado))
         return
 
       else:
@@ -52,11 +64,13 @@ def Op():
     case '5':
 
      Resultado = (x**y)
+     print('\n' + Number_Format(Resultado))
      return
 
     case '6':
 
      Resultado = (m.sqrt(int(x)))
+     print('\n' + Number_Format(Resultado))
      return
 
     case '7':
@@ -64,6 +78,7 @@ def Op():
      if x >= 0:
 
       Resultado = (m.factorial(int(x)))
+      print('\n' + Number_Format(Resultado))
       return
 
      else:
@@ -74,16 +89,19 @@ def Op():
     case '8':
 
      Resultado = (Triangle_Number(x))
+     print('\n' + Number_Format(Resultado))
      return
 
     case '9':
 
      Resultado = (x==y)
+     print('\n' + Number_Format(Resultado))
      return
 
     case '10':
 
      Resultado = (x!=y)
+     print('\n' + Number_Format(Resultado))
      return
 
     case _:
@@ -93,7 +111,8 @@ def Op():
 
 while True:
  Op()
- Again = input('\nReiniciar? (Y/N): ').strip().upper()
+ print()
+ Again = input('Reiniciar? (Y/N): ').strip().upper()
  if Again != 'Y':
-  print('\nTchau!')
+  print('\nTchau!\n')
   break
