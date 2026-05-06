af478c461e6ed57952f50b4b15c00f3547af9a81
Victor Augusto <victor.ribeiro.silva30@escola.pr.gov.br>
2026-05-06 07:49:10 -0300

    e

--- Exercícios/Exercício 10.py
+++ Exercícios/Exercício 10.py
@@ -2,5 +2,5 @@ def Name_Verify (Name):
     return "Sim!" if 'silva' in Name.lower() else "Não!"
 
 while True: 
- Name = input(str('Qual é o seu nome completo? ')).strip()
- print(f'Seu nome tem silva? {Name_Verify(Name)}')
+ Name = input(str('\nQual é o seu nome completo? ')).strip()
+ print(f'\nSeu nome tem silva? {Name_Verify(Name)}')
